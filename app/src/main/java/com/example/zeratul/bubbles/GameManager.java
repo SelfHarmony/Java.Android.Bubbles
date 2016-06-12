4f5b04d09c31bbd18805a05833b553c2db801d31
SelfHarmony <tdnttd@mail.ru>
2016-06-12 19:51:32 +0400

    Создаём коллекцию вражеских кругов. Они пока одного цвета с главным кругом.

--- app/src/main/java/com/example/zeratul/bubbles/GameManager.java
+++ app/src/main/java/com/example/zeratul/bubbles/GameManager.java
@@ -1,7 +1,11 @@
 package com.example.zeratul.bubbles;
 
+import java.util.ArrayList;
+
 public class GameManager {
+    public static final int MAX_CIRCLES = 10;
     private MainCircle mainCircle;
+    private ArrayList<EnemyCircle> circles;
     private CanvasView canvasView;
     private static int width;
     private static int height;
@@ -11,7 +15,17 @@ public class GameManager {
         width = w;
         height = h;
         initMainCircle();
+        initEnemyCircles();
 
+    }
+
+    private void initEnemyCircles() {
+        circles = new ArrayList<EnemyCircle>();
+        for (int i = 0; i < MAX_CIRCLES; i++) {
+            EnemyCircle circle;
+            circle = EnemyCircle.getRandomCircle();
+            circles.add(circle);
+        }
     }
 
     public static int getWidth() {
@@ -29,6 +43,10 @@ public class GameManager {
 
     public void onDraw() {
         canvasView.drawCircle(mainCircle);
+        for (EnemyCircle circle : circles) {
+            canvasView.drawCircle(circle);
+            
+        }
     }
 
 
